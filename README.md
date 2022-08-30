0e8e0f30cd2754e3aa2b9d4820789fe4bf0bcd10
Dominik Zietko <mim1q.dev@gmail.com>
2022-08-30 14:31:01 +0200

    Buildscript created

---
+++ README.md
@@ -0,0 +1,1 @@
+# Demolished Minecraft Mod
---
+++ buildSrc/src/main/kotlin/ModData.kt
@@ -0,0 +1,11 @@
+object ModData {
+  const val group = "com.github.mim1q"
+  const val id = "demolished"
+  const val version = "0.1.0"
+  const val versionType = "alpha"
+  val mcVersions = listOf("1.19", "1.19.1", "1.19.2")
+  val dependencies = listOf("fabric-api")
+  val changelog = """
+    
+  """.trimIndent()
+}
